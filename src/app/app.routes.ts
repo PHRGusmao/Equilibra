71e4d43fc8e0e194d978b313521fd773c4b7faa4
PHRGusmao <phrgusmao@gmail.com>
2025-05-11 00:13:09 -0300

    feat: desenvolvimento html landing-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+        path:"equilibra",
+        component: LandingPageComponent
+  }
+];
